8b6dd525f29465292be643ff49773b1190805359
SueTheCoder <suheyla315@hotmail.com>
2021-10-08 15:16:28 -0400

    getStudentWithID() test and Library.java library_base.java added

--- src/test/java/com/tests/Library.java
+++ src/test/java/com/tests/Library.java
@@ -5,12 +5,19 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
 
+import static io.restassured.RestAssured.*;
+import static org.hamcrest.MatcherAssert.*;
+import static org.hamcrest.Matchers.*;
+import static org.junit.jupiter.api.Assertions.*;
+
 public class Library implements library_base {
 
     @Tag("smoke")
     @Test
     @DisplayName("get_book_categories")
     public void get_book_categories(){
-
+        given().header("x-library-token", library_base.getToken())
+                .when().get("/get_book_categories").prettyPeek()
+                .then().statusCode(200).contentType("application/json; charset=utf-8");
     }
 }
